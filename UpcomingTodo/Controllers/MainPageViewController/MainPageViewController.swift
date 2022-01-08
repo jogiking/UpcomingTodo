784ab696c9aa1cb8a2662e1049ad73d7cdecaa71
jogiking <whrldhks38@gmail.com>
2022-01-09 01:45:30 +0900

    chore : delegate 파일 분리

--- UpcomingTodo/Controllers/MainPageViewController/MainPageViewController.swift
+++ UpcomingTodo/Controllers/MainPageViewController/MainPageViewController.swift
@@ -325,17 +325,3 @@ class MainPageViewController: UIViewController {
     }
     
 }
-
-extension MainPageViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        guard let navigationController = self.navigationController else { return }
-        let threshold = navigationController.navigationBar.frame.height
-        print("threshold = \(threshold)")
-        let alpha = scrollView.contentOffset.y / threshold
-        navigationController.navigationBar.subviews.first?.alpha = alpha
-        
-        updateNavigationTitle()
-
-        print("didScroll] \(scrollView.contentOffset.y)")
-    }
-}
