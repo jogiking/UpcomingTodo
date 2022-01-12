f1681c3291990d2914c0a94d63de662eb625b2d2
jogiking <whrldhks38@gmail.com>
2022-01-12 23:52:10 +0900

    chore : TableView Extension 분리

--- UpcomingTodo/Controllers/TodoListViewController/TodoListViewController.swift
+++ UpcomingTodo/Controllers/TodoListViewController/TodoListViewController.swift
@@ -380,16 +380,6 @@ class TodoListViewController: UIViewController, TodoDetailViewControllerDelegate
     }
 }
 
-extension UITableView {
-
-    func setBottomInset(to value: CGFloat) {
-        let edgeInset = UIEdgeInsets(top: 0, left: 0, bottom: value, right: 0)
-
-        self.contentInset = edgeInset
-        self.scrollIndicatorInsets = edgeInset
-    }
-}
-
 extension TodoListViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
      
